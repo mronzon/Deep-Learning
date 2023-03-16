303a91cb636e3a033bf8e52fd9902ecccb475acc
mronzon <104356392+mronzon@users.noreply.github.com>
2023-03-16 11:37:35 -0400

    Make the player movement

---
+++ Assets/Script/CarMovement/UserMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script.CarMovement
+{
+    
+    public class UserMovement : MonoBehaviour
+    {
+        public int speed = 2;
+
+        public float rotationValue = 30;
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = gameObject.GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            float horizontalMovement = Input.GetAxis("Horizontal");
+            float verticalMovement = Input.GetAxis("Vertical");
+
+            Vector3 input = new Vector3(verticalMovement, 0, 0);
+            transform.Translate(input * Time.deltaTime * speed);
+            if (verticalMovement != 0)
+            {
+                Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, rotationValue * horizontalMovement, 0) * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotation);
+            }
+            
+            
+        }
+
+    }
+}
